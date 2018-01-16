7b6b244e6ca542d38bd3b4c85bfbf1e513909c6f
ouqunfu <ouqunfu@foxmail.com>
2018-01-16 15:21:28 +0800

    增加过滤文件；菜单添加适当icon

--- src/router/router.js
+++ src/router/router.js
@@ -82,72 +82,57 @@ export const otherRouter = {
 // 作为Main组件的子页面展示并且在左侧菜单显示的路由写在appRouter里
 export const appRouter = [
   {
-    path: '/index1',
-    icon: 'ios-paper',
-    name: 'home',
-    title: '首页',
-    component: Main,
-    children: [
-      {
-        path: 'index1',
-        name: 'index1',
-        title: '首页',
-        component: resolve => { require(['@/views/home/home.vue'], resolve) }
-      }
-    ]
-  },
-  {
     path: '/contents',
-    icon: 'ios-folder',
+    icon: 'filing',
     name: 'contents',
     title: '内容',
     component: Main,
     children: [
       {
         path: 'columns-list',
-        icon: 'ios-paper-outline',
+        icon: 'ios-list-outline',
         name: 'columns-list',
         title: '栏目管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'cato-list',
-        icon: 'ios-list-outline',
+        icon: 'ios-list',
         name: 'cato-list',
         title: '分类管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'subjects-list',
-        icon: 'ios-list-outline',
+        icon: 'folder',
         name: 'subjects-list',
         title: '专题管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'contents-list',
-        icon: 'ios-list-outline',
+        icon: 'document-text',
         name: 'contents-list',
         title: '内容管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'url-manage-list',
-        icon: 'ios-list-outline',
+        icon: 'link',
         name: 'url-manage-list',
         title: 'URL规则',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'gallery-list-dir',
-        icon: 'ios-list-outline',
+        icon: 'android-image',
         name: 'gallery-list-dir',
         title: '图片库',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'tags-list',
-        icon: 'ios-list-outline',
+        icon: 'ios-pricetags',
         name: 'tags-list',
         title: '标签管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
@@ -156,21 +141,21 @@ export const appRouter = [
   },
   {
     path: '/users',
-    icon: 'ios-folder',
+    icon: 'ios-people',
     name: 'users',
     title: '用户',
     component: Main,
     children: [
       {
         path: 'users-list',
-        icon: 'ios-paper-outline',
+        icon: 'ios-personadd',
         name: 'users-list',
         title: '用户管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'role-list',
-        icon: 'ios-list-outline',
+        icon: 'person-stalker',
         name: 'role-list',
         title: '角色管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
@@ -179,35 +164,35 @@ export const appRouter = [
   },
   {
     path: '/functions',
-    icon: 'ios-folder',
+    icon: 'android-apps',
     name: 'functions',
     title: '功能',
     component: Main,
     children: [
       {
         path: 'list-notice',
-        icon: 'ios-paper-outline',
+        icon: 'clipboard',
         name: 'list-notice',
         title: '公告管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'list-message',
-        icon: 'ios-list-outline',
+        icon: 'chatbubble',
         name: 'list-message',
         title: '留言管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'comments-list',
-        icon: 'ios-paper-outline',
+        icon: 'chatbox',
         name: 'comments-list',
         title: '评论管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'data-manage',
-        icon: 'ios-list-outline',
+        icon: 'social-buffer',
         name: 'data-manage',
         title: '数据管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
@@ -216,21 +201,21 @@ export const appRouter = [
   },
   {
     path: '/market',
-    icon: 'ios-folder',
+    icon: 'connection-bars',
     name: 'market',
     title: '营销',
     component: Main,
     children: [
       {
         path: 'list-seo-rule',
-        icon: 'ios-paper-outline',
+        icon: 'arrow-graph-up-right',
         name: 'list-seo-rule',
         title: 'SEO管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'list-links',
-        icon: 'ios-list-outline',
+        icon: 'link',
         name: 'list-links',
         title: '友链管理',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
@@ -239,35 +224,35 @@ export const appRouter = [
   },
   {
     path: '/settings',
-    icon: 'ios-folder',
+    icon: 'android-settings',
     name: 'settings',
     title: '设置',
     component: Main,
     children: [
       {
         path: 'site-config',
-        icon: 'ios-paper-outline',
+        icon: 'ios-world-outline',
         name: 'site-config',
         title: '网站配置',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'watermark',
-        icon: 'ios-list-outline',
+        icon: 'bookmark',
         name: 'watermark',
         title: '水印设置',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
       },
       {
         path: 'str-filter-list',
-        icon: 'ios-paper-outline',
+        icon: 'ios-color-filter-outline',
         name: 'str-filter-list',
         title: '敏感词管理',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'attachment',
-        icon: 'ios-list-outline',
+        icon: 'android-attach',
         name: 'attachment',
         title: '附件设置',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
@@ -276,21 +261,21 @@ export const appRouter = [
   },
   {
     path: '/customs',
-    icon: 'ios-folder',
+    icon: 'ios-compose-outline',
     name: 'customs',
     title: '自定义',
     component: Main,
     children: [
       {
         path: 'list-content-field',
-        icon: 'ios-paper-outline',
+        icon: 'edit',
         name: 'list-content-field',
         title: '内容字段',
         component: resolve => { require(['@/views/group/page1/page1.vue'], resolve) }
       },
       {
         path: 'list-user-field',
-        icon: 'ios-list-outline',
+        icon: 'edit',
         name: 'list-user-field',
         title: '用户字段',
         component: resolve => { require(['@/views/group/page2/page2.vue'], resolve) }
